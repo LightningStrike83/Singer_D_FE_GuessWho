370190691559f56f015a013442d225c79386b8f1
Dan Singer <d_singer@fanshaweonline.ca>
2024-10-30 22:30:04 -0400

    Guess Who Setup
    -Added files for Lumen
    -Edited lumen files for functionality
    -Added base elements for webpage
    -Edited stylings for webpage

---
+++ lumen/app/Http/Controllers/CharacterController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Character;
+
+
+class CharacterController extends Controller {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    public function getAll() {
+        $characters = Character::select('id', 'name', 'game')->orderBy('name', 'asc')->get();
+        return response()->json($characters);
+    }
+
+     public function getOne($game){
+        $characters = Character::select('id', 'name', 'game')->where('game', '=', $game)->orderBy('name', 'asc')->get();
+        return response()->json($characters);
+    }
+}
+
+
+
---
+++ lumen/app/Http/Controllers/GuessWhoGameController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\GuessWhoGame;
+
+
+class GuessWhoGameController extends Controller {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+     public function getOne($id){
+        $guesswho = GuessWhoGame::where('id', $id)->first();
+        
+        if (!$guesswho) {
+            return response()->json(['error' => 'GuessWhoGame not found'], 404);
+        }
+        
+        $characterData = [];
+        
+        for ($i = 1; $i <= 25; $i++) {
+            $characterNumber = $guesswho->{"character$i"};
+            $character = CharacterController::where('number', $characterNumber)->first();
+            
+            if ($character) {
+                $characterData[] = [
+                    'number' => $character->number,
+                    'name' => $character->name
+                ];
+            }
+        }
+        
+        return response()->json($characterData);
+    }
+    
+
+    public function save(Request $request) {
+        $this->validate($request, [
+            'character1' => 'required',
+            'character2' => 'required',
+            'character3' => 'required',
+            'character4' => 'required',
+            'character5' => 'required',
+            'character6' => 'required',
+            'character7' => 'required',
+            'character8' => 'required',
+            'character9' => 'required',
+            'character10' => 'required',
+            'character11' => 'required',
+            'character12' => 'required',
+            'character13' => 'required',
+            'character14' => 'required',
+            'character15' => 'required',
+            'character16' => 'required',
+            'character17' => 'required',
+            'character18' => 'required',
+            'character19' => 'required',
+            'character20' => 'required',
+            'character21' => 'required',
+            'character22' => 'required',
+            'character23' => 'required',
+            'character24' => 'required',
+            'character25' => 'required',
+        ]);
+        $guesswho = GuessWhoGame::create($request->all());
+        return response()->json($guesswho, 201);
+    }
+}
+
